54811ccbf8efc3525663eb39a75cc1d3bd011e0d
luisguillermomolero <luisguillermomolero@gmail.com>
2021-08-10 11:04:39 -0500

    Six commit

--- Modulo2_Java_MisionTIC2022_Main/Semana_5/01_Ejemplos/01_ejercicio_uno/src/main/java/semana_cinco_ejercicios/App.java
+++ Modulo2_Java_MisionTIC2022_Main/Semana_5/01_Ejemplos/01_ejercicio_uno/src/main/java/semana_cinco_ejercicios/App.java
@@ -4,12 +4,12 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) {
-        JFrame marco = new JFrame();// creating instance of JFrame
+        JFrame marco = new JFrame();// Creando uns instancia con jframe
 
-        JButton boton = new JButton("Clic");// creating instance of JButton
+        JButton boton = new JButton("Clic");// Creando una instancia con JButton
         boton.setBounds(130, 100, 100, 40);// x axis, y axis, width, height
 
-        marco.add(boton);// adding button in JFrame
+        marco.add(boton);// Agregando un botón dentro del JFrame
 
         marco.setSize(400, 500);// 400 width and 500 height
         marco.setLayout(null);// using no layout managers
